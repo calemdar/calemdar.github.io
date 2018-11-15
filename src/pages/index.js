458b99ad3e998c2e81650b302252848a0cf51a14
calemdar <calemdar@wpi.edu>
2018-11-15 15:05:09 -0500

    Font and color

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,9 @@ import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Cem Alemdar</h1>
+    <p>Here is my portfolio</p>
+    <p>Please hire me!!</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
